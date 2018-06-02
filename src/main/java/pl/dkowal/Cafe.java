eb93d28f429ab57938899ce3bbf2015c1288a0db
Dawid <dawid.k.kowal@gmail.com>
2018-06-02 08:48:18 +0200

    first tests

---
+++ src/main/java/pl/dkowal/Cafe.java
@@ -0,0 +1,39 @@
+package pl.dkowal;
+
+public class Cafe {
+    private int beansInStock = 0;
+    private int milkInStock = 0;
+
+    public Coffee brew(CoffeeType coffeeType) {
+        return brew(coffeeType, 1);
+    }
+
+    public Coffee brew(CoffeeType coffeeType, int quantity) {
+        int requiredBeans = coffeeType.getRequiredBeans() * quantity;
+        int requiredMilk = coffeeType.getRequiredMilk() * quantity;
+        if(requiredBeans > beansInStock || requiredMilk > milkInStock)
+            throw new IllegalStateException("Insufficient beans or milk");
+
+        beansInStock -= requiredBeans;
+        milkInStock -= requiredMilk;
+        return new Coffee(coffeeType, requiredBeans, requiredMilk);
+    }
+
+    public void restockBeans(int weightInGrams) {
+        requirePositive(weightInGrams);
+        beansInStock += weightInGrams;
+    }
+
+    private void requirePositive(int value) {
+        if (value < 1)
+            throw new IllegalArgumentException();
+    }
+
+    public int getBeansInStock() {
+        return beansInStock;
+    }
+
+    public int getMilkInStock() {
+        return milkInStock;
+    }
+}
